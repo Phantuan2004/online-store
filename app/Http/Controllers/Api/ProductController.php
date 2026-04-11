6fccd19a8f358170c37308184d745674e93a1f2d
phantuan2004 <phanhoangtuan21072004@gmail.com>
2026-04-11 22:20:14 +0700

    edit paginate product from 15 to 12.

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -14,7 +14,7 @@ class ProductController extends Controller
 {
     public function index()
     {
-        $products = Product::with(['images', 'image', 'variants.image', 'variants.attributeValues.attribute', 'category'])->paginate(15);
+        $products = Product::with(['images', 'image', 'variants.image', 'variants.attributeValues.attribute', 'category'])->paginate(12);
         return ProductResource::collection($products);
     }
 
